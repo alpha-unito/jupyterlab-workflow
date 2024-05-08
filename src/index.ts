6a602510052755056dddee323e2ae43bc7669e30
Pier2690 <156895742+Pier2690@users.noreply.github.com>
2024-05-08 12:53:34 +0200

    Hello jupyterlab Workflow

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,28 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { DOMUtils } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets';
+
+const TOP_AREA_CSS_CLASS = 'jp-TopArea';
 
 /**
  * Initialization data for the jupyterlab_workflow extension.
  */
+
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_workflow:plugin',
-  description: 'Manage and modify HPC workflows directly from your jupyterLab environment with an intuitive interface.',
+  description:
+    'Manage and modify HPC workflows directly from your jupyterLab environment with an intuitive interface.',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab_workflow is activated!');
+    console.log('jupyterlab_workflow is activated!');
+    const node = document.createElement('div');
+    node.textContent = 'Hello JupyterLab Workflow!';
+    const widget = new Widget({ node });
+    widget.id = DOMUtils.createDomID();
+    widget.addClass(TOP_AREA_CSS_CLASS);
+    app.shell.add(widget, 'top', { rank: 1000 });
   }
 };
 
